e4157e42304d97a79ee5f8932ccd5e562f6354f6
Mayhew Seavey <scorpy@gmail.com>
2015-08-26 17:42:07 -0700

    Change expected field names

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', function(request, response) {
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT game FROM games', function(err, result) {
+    client.query('SELECT id, game FROM games', function(err, result) {
       done();
       if (err)
       { console.error(err); response.send("Error " + err); }
