fe56dbd1ff051d15c819b3cb3c432ed13b339293
Mayhew Seavey <scorpy@gmail.com>
2015-08-29 22:32:00 -0700

    Update button now updates the model (not the DB)

--- public/javascripts/gameDetailController.js
+++ public/javascripts/gameDetailController.js
@@ -5,7 +5,11 @@ angular.module('mediaMogulApp')
 
     self.game = game;
 
-
+    if (game.logo == null || game.logo == '') {
+      game.imageUrl = "images/trans.gif";
+    } else {
+      game.imageUrl = "http://media.steampowered.com/steamcommunity/public/images/apps/" + game.steamid + "/" + game.logo + ".jpg";
+    }
 
     self.originalFields = {
       Platform: self.game.platform,
@@ -15,7 +19,6 @@ angular.module('mediaMogulApp')
       Mayhew: self.game.mayhew,
       TimePlayed: self.game.timeplayed,
       TimeTotal: self.game.timetotal,
-      Finished: self.game.finished,
       FinalScore: self.game.finalscore,
       Replay: self.game.replay
     };
@@ -28,15 +31,22 @@ angular.module('mediaMogulApp')
       Mayhew: self.game.mayhew,
       TimePlayed: self.game.timeplayed,
       TimeTotal: self.game.timetotal,
-      Finished: self.game.finished,
       FinalScore: self.game.finalscore,
       Replay: self.game.replay
     };
 
-    $log.debug("Game opened: " + game.game + ", Finished: " + self.interfaceFields.Finished);
+    $log.debug("Game opened: " + game.game + ", Finished: " + self.game.finished);
 
     self.changeValues = function() {
-
+      self.game.platform = self.interfaceFields.Platform;
+      self.game.owned = self.interfaceFields.Owned;
+      self.game.metacritic = self.interfaceFields.Metacritic;
+      self.game.metacritic_hint = self.interfaceFields.MetacriticHint;
+      self.game.mayhew = self.interfaceFields.Mayhew;
+      self.game.timeplayed = self.interfaceFields.TimePlayed;
+      self.game.timetotal = self.interfaceFields.TimeTotal;
+      self.game.finalscore = self.interfaceFields.FinalScore;
+      self.game.replay = self.interfaceFields.Replay;
     };
 
     self.ok = function() {
