06001a364f1154ccaa0bf6662932696b777467a1
Mayhew Seavey <scorpy@gmail.com>
2016-05-16 21:35:21 -0700

    MM-109: Change to cookie-session instead of server-side store. This should be able to survive server restarts.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var app = express();
@@ -9,14 +10,15 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cookieParser());
-var sessionMiddleware = session({
-  resave: false,
-  saveUninitialized: true,
-  cookie:{httpOnly: true, maxAge: 1000*3600*24*30}, //remember for 30 days
-  secret: process.env.MEDIAMOGULSECRET
-});
-app.use(sessionMiddleware);
+app.use(cookieParser(process.env.MEDIAMOGULSECRET));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1'],
+  cookie:{
+    httpOnly: true,
+    maxAge: 1000*3600*24*30
+  }
+}));
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 require('./routes/routes.js')(app);
