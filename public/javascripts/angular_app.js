801911ef5a648ad88791f95f0ebdf08227e92959
Mayhew Seavey <scorpy@gmail.com>
2016-08-08 19:36:09 -0700

    MM-231: Add necessary scaffolding to get Auth0 to work. Changed ng-route to link controller instead of HTML file, to follow pattern in example.

--- public/javascripts/angular_app.js
+++ public/javascripts/angular_app.js
@@ -2,6 +2,7 @@ angular.module('mediaMogulApp', ['ngRoute', 'ui.bootstrap'])
   .config(function($routeProvider) {
     $routeProvider
       .when('/', {
+        controller: 'gamesController as ctrl',
         templateUrl: 'views/games.html'
       })
       .when('/login', {
