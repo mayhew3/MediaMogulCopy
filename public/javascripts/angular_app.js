2b9f35731d34fb14b8baa15559ced20e16689284
Mayhew Seavey <scorpy@gmail.com>
2016-08-09 22:43:44 -0700

    MM-231: Quick and dirty login screen. It allows a login, but doesn't restrict permissions or anything.

--- public/javascripts/angular_app.js
+++ public/javascripts/angular_app.js
@@ -1,15 +1,55 @@
-angular.module('mediaMogulApp', ['ngRoute', 'ui.bootstrap'])
-  .config(function($routeProvider) {
-    $routeProvider
-      .when('/', {
-        controller: 'gamesController as ctrl',
-        templateUrl: 'views/games.html'
-      })
-      .when('/login', {
-        templateUrl: 'views/login.html'
-      })
-    ;
-    $routeProvider.otherwise({
-      redirectTo: '/'
-    });
-  });
+angular.module('mediaMogulApp', ['auth0', 'angular-storage', 'angular-jwt', 'ngRoute', 'ui.bootstrap'])
+  .config(['$routeProvider', 'authProvider', '$httpProvider', '$locationProvider', 'jwtInterceptorProvider',
+    function($routeProvider, authProvider, $httpProvider, $locationProvider, jwtInterceptorProvider) {
+      $routeProvider
+        .when('/', {
+          controller: 'gamesController as ctrl',
+          templateUrl: 'views/games.html'
+        })
+        .when('/login', {
+          controller: 'LoginCtrl',
+          templateUrl: 'views/login.html',
+          pageTitle: 'Login'
+        })
+      ;
+      $routeProvider.otherwise({
+        redirectTo: '/'
+      });
+
+      authProvider.init({
+        domain: '',
+        clientID: '',
+        loginUrl: '/login'
+      });
+
+      //Called when login is successful
+      authProvider.on('loginSuccess', ['$location', 'profilePromise', 'idToken', 'store',
+        function($location, profilePromise, idToken, store) {
+
+          console.log("Login Success");
+          profilePromise.then(function(profile) {
+            store.set('profile', profile);
+            store.set('token', idToken);
+          });
+
+          $location.path('/');
+        }]);
+
+      //Called when login fails
+      authProvider.on('loginFailure', function() {
+        alert("Error");
+      });
+
+      //Angular HTTP Interceptor function
+      jwtInterceptorProvider.tokenGetter = ['store', function(store) {
+        return store.get('token');
+      }];
+
+      //Push interceptor function to $httpProvider's interceptors
+      $httpProvider.interceptors.push('jwtInterceptor');
+
+    }])
+  .run(['auth', function(auth) {
+    auth.hookEvents();
+  }])
+;
