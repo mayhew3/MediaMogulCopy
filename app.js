c5d659aa7f62b06e72a1163a750f3b2e6c38451c
Mayhew Seavey <scorpy@gmail.com>
2016-08-12 20:13:05 -0700

    MM-249: Add favicon

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var cookieSession = require('cookie-session');
 var bodyParser = require('body-parser');
 var cors = require('cors');
-var session = require('express-session');
+var favicon = require('serve-favicon');
 var app = express();
 
+app.use(favicon(__dirname + '/public/images/favicon.ico', {}));
 app.use(logger('dev'));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
