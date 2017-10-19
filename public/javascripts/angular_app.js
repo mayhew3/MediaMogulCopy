2780406a27e93523ca1b9e9936b02ffae40df712
Mayhew Seavey <mseavey@guidewire.com>
2017-10-19 13:55:51 -0700

    MM-350: Update Auth0 back to the special Heroku Auth0.

--- public/javascripts/angular_app.js
+++ public/javascripts/angular_app.js
@@ -92,8 +92,8 @@ angular.module('mediaMogulApp', ['auth0', 'angular-storage', 'angular-jwt', 'ngR
       ;
 
       authProvider.init({
-        domain: 'mayhew3.auth0.com',
-        clientID: 'QdwQv7LcXgmiUpYhXnTYyGQsXie2UQNb'
+        domain: 'app40365095.auth0.com',
+        clientID: 'dSHWdaxORGRtRUFi8WdGCzlECwAmSxF4'
       });
 
       function redirect($q, $injector, $timeout, store, $location) {
