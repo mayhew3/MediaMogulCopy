aa0ac27f787412d836f46b0a58a6f1e388b242b4
Mayhew Seavey <mseavey@guidewire.com>
2017-10-20 15:20:18 -0700

    MM-145: More refactoring. Remove all the extra parameters.

--- public/javascripts/toolbarController.js
+++ public/javascripts/toolbarController.js
@@ -32,10 +32,8 @@
         store.set('token', idToken);
         store.set('accessToken', accessToken);
         store.set('refreshToken', refreshToken);
-        // console.log("ID Token: " + idToken);
-        // console.log("Refresh Token: " + refreshToken);
-        syncPersonWithDB(profile, $http, self.auth, $location);
-
+        syncPersonWithDB(profile);
+        
       }, function(error) {
         console.log(error);
       })
@@ -52,56 +50,58 @@
       self.auth.roles = [];
       $location.path('/');
     }
-  }
 
-  // user management functions
 
-  function syncPersonWithDB(profile, $http, auth, $location) {
-    var email = profile.email;
+    // user management functions
 
-    $http.get('/person', {params: {email: email}}).then(function (response) {
-      var personData = response.data;
-      console.log("User info found: " + personData.length + " rows.");
+    function syncPersonWithDB(profile) {
+      var email = profile.email;
 
-      if (personData.length === 0) {
-        addPersonToDB(profile, $http, email, auth);
-      } else {
-        copyPersonInfoToAuth(personData, auth);
-      }
+      $http.get('/person', {params: {email: email}}).then(function (response) {
+        var personData = response.data;
+        console.log("User info found: " + personData.length + " rows.");
 
-      auth.roles = profile.app_metadata.roles;
-      auth.isAdmin = function () {
-        return this.isAuthenticated && _.contains(this.roles, 'admin');
-      };
-      $location.path('/tv/shows/main');
-    });
-  }
+        if (personData.length === 0) {
+          addPersonToDB(profile);
+        } else {
+          copyPersonInfoToAuth(personData);
+        }
 
-  function addPersonToDB(profile, $http, email, auth) {
-    console.log("No person found. Adding: " + profile.user_metadata.first_name);
-    var user_metadata = profile.user_metadata;
-    $http.post('/addPerson', {
-      Person: {
-        email: email,
-        first_name: user_metadata.first_name,
-        last_name: user_metadata.last_name
-      }
-    }).then(function (response) {
-      console.log("Added successfully. Person ID: " + response.data.PersonId);
-      auth.person_id = response.data.PersonId;
-    }, function (err) {
-      console.log("Error adding person to DB: " + err);
-    });
-  }
+        self.auth.roles = profile.app_metadata.roles;
+        self.auth.isAdmin = function () {
+          return this.isAuthenticated && _.contains(this.roles, 'admin');
+        };
+        $location.path('/tv/shows/main');
+      });
+    }
 
-  function copyPersonInfoToAuth(personData, auth) {
-    var personInfo = personData[0];
-    console.log("Name: " + personInfo.first_name + " " + personInfo.last_name);
-    console.log("ID: " + personInfo.id);
+    function addPersonToDB(profile) {
+      console.log("No person found. Adding: " + profile.user_metadata.first_name);
+      var user_metadata = profile.user_metadata;
+      $http.post('/addPerson', {
+        Person: {
+          email: profile.email,
+          first_name: user_metadata.first_name,
+          last_name: user_metadata.last_name
+        }
+      }).then(function (response) {
+        console.log("Added successfully. Person ID: " + response.data.PersonId);
+        self.auth.person_id = response.data.PersonId;
+      }, function (err) {
+        console.log("Error adding person to DB: " + err);
+      });
+    }
 
-    auth.firstName = personInfo.first_name;
-    auth.lastName = personInfo.last_name;
-    auth.person_id = personInfo.id;
+    function copyPersonInfoToAuth(personData) {
+      var personInfo = personData[0];
+      console.log("Name: " + personInfo.first_name + " " + personInfo.last_name);
+      console.log("ID: " + personInfo.id);
+
+      self.auth.firstName = personInfo.first_name;
+      self.auth.lastName = personInfo.last_name;
+      self.auth.person_id = personInfo.id;
+    }
+
   }
 
 })();
