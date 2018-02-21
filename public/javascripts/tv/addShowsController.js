4e399161d188c915a47a0d2c8c32865a6cb55965
Mayhew Seavey <commander.boffo@gmail.com>
2018-02-20 19:34:22 -0800

    MM-467: Don't see any punctuation issues... I wonder if it was because of caps? Making the search case agnostic in case.

--- public/javascripts/tv/addShowsController.js
+++ public/javascripts/tv/addShowsController.js
@@ -2,7 +2,7 @@ angular.module('mediaMogulApp')
   .filter("filterByTitle", function() {
     return function (shows, titleFragment) {
       return _.filter(shows, function(show) {
-        return titleFragment === undefined || show.title.indexOf(titleFragment) > -1;
+        return titleFragment === undefined || show.title.toLowerCase().indexOf(titleFragment.toLowerCase()) > -1;
       });
     }
   })
