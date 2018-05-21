3b84ed7a3839215a6437f1d4ffe44844f9f0541c
Mayhew Seavey <commander.boffo@gmail.com>
2018-05-21 15:20:47 -0700

    MM-85: Add Playtime modal with hours. Don't love the layout, but this is partially there. Gonna try upgrading to Bootstrap 4.

--- public/javascripts/games/addPlaytimeController.js
+++ public/javascripts/games/addPlaytimeController.js
@@ -1,6 +1,6 @@
 angular.module('mediaMogulApp')
-.controller('gameDetailController', ['$log', 'GamesService', '$uibModalInstance', 'game', 'auth', '$uibModal',
-  function($log, GamesService, $uibModalInstance, game, auth, $uibModal) {
+.controller('addPlaytimeController', ['$log', 'GamesService', '$uibModalInstance', 'game', 'auth',
+  function($log, GamesService, $uibModalInstance, game, auth) {
     var self = this;
 
     self.auth = auth;
@@ -39,39 +39,29 @@ angular.module('mediaMogulApp')
 
     // FIELDS
 
+    self.original_hours = null;
+    self.original_minutes = null;
+    self.new_hours = null;
+    self.new_minutes = null;
+    self.added_hours = null;
+    self.added_minutes = null;
+    self.session_rating = null;
+    self.finished = false;
+
+
     self.originalFields = {
-      platform: self.game.platform,
-      owned: self.game.owned,
-      metacritic: self.game.metacritic,
-      metacritic_hint: self.game.metacritic_hint,
-      guess: self.game.guess,
-      mayhew: self.game.mayhew,
       timeplayed: self.game.timeplayed,
       timetotal: self.game.timetotal,
-      natural_end: self.game.natural_end,
       finalscore: self.game.finalscore,
       replay: self.game.replay,
-      howlong_id: self.game.howlong_id,
-      giantbomb_manual_guess: self.game.giantbomb_manual_guess,
-      giantbomb_id: self.game.giantbomb_id,
       finished: self.game.finished === null ? null : new Date(self.game.finished).toLocaleDateString("en-US", options)
     };
 
     self.interfaceFields = {
-      platform: self.game.platform,
-      owned: self.game.owned,
-      metacritic: self.game.metacritic,
-      metacritic_hint: self.game.metacritic_hint,
-      guess: self.game.guess,
-      mayhew: self.game.mayhew,
       timeplayed: self.game.timeplayed,
       timetotal: self.game.timetotal,
-      natural_end: self.game.natural_end,
       finalscore: self.game.finalscore,
       replay: self.game.replay,
-      howlong_id: self.game.howlong_id,
-      giantbomb_manual_guess: self.game.giantbomb_manual_guess,
-      giantbomb_id: self.game.giantbomb_id,
       finished: self.game.finished === null ? null : new Date(self.game.finished).toLocaleDateString("en-US", options)
     };
 
@@ -104,35 +94,15 @@ angular.module('mediaMogulApp')
 
         GamesService.updateGame(game.id, changedFields).then(function() {
           // todo: loop?
-          self.game.platform = self.interfaceFields.platform;
-          self.game.owned = self.interfaceFields.owned;
-          self.game.metacritic = self.interfaceFields.metacritic;
-          self.game.metacritic_hint = self.interfaceFields.metacritic_hint;
-          self.game.guess = self.interfaceFields.guess;
-          self.game.mayhew = self.interfaceFields.mayhew;
           self.game.timeplayed = self.interfaceFields.timeplayed;
           self.game.timetotal = self.interfaceFields.timetotal;
-          self.game.natural_end = self.interfaceFields.natural_end;
           self.game.finalscore = self.interfaceFields.finalscore;
           self.game.replay = self.interfaceFields.replay;
-          self.game.howlong_id = self.interfaceFields.howlong_id;
-          self.game.giantbomb_manual_guess = self.interfaceFields.giantbomb_manual_guess;
-          self.game.giantbomb_id = self.interfaceFields.giantbomb_id;
 
-          self.originalFields.platform = self.interfaceFields.platform;
-          self.originalFields.owned = self.interfaceFields.owned;
-          self.originalFields.metacritic = self.interfaceFields.metacritic;
-          self.originalFields.metacritic_hint = self.interfaceFields.metacritic_hint;
-          self.originalFields.guess = self.interfaceFields.guess;
-          self.originalFields.mayhew = self.interfaceFields.mayhew;
           self.originalFields.timeplayed = self.interfaceFields.timeplayed;
           self.originalFields.timetotal = self.interfaceFields.timetotal;
-          self.originalFields.natural_end = self.interfaceFields.natural_end;
           self.originalFields.finalscore = self.interfaceFields.finalscore;
           self.originalFields.replay = self.interfaceFields.replay;
-          self.originalFields.howlong_id = self.interfaceFields.howlong_id;
-          self.originalFields.giantbomb_id = self.interfaceFields.giantbomb_id;
-          self.originalFields.giantbomb_manual_guess = self.interfaceFields.giantbomb_manual_guess;
 
           GamesService.updatePlaytimes(game);
           GamesService.updateRating(game);
